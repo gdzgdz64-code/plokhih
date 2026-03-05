63d0174a99a8a8138641555c915556ace8c90ae3
politolog <gdzgdz64@gmail.com>
2026-03-05 20:36:43 +0300

    Add files via upload

---
+++ api/auth.js
@@ -0,0 +1,8 @@
+const { createVercelBeginHandler } = require('netlify-cms-oauth-provider-node');
+
+module.exports = createVercelBeginHandler(
+  {},
+  {
+    useEnv: true,
+  }
+);
---
+++ api/callback.js
@@ -0,0 +1,8 @@
+const { createVercelCompleteHandler } = require('netlify-cms-oauth-provider-node');
+
+module.exports = createVercelCompleteHandler(
+  {},
+  {
+    useEnv: true,
+  }
+);
